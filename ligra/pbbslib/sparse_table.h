3af48d05975fbdd5ec3a093d4bcd665139b438dc
Laxman Dhulipala <laxmandhulipala@gmail.com>
2019-12-15 20:23:50 -0500

    warning

--- ligra/pbbslib/sparse_table.h
+++ ligra/pbbslib/sparse_table.h
@@ -85,11 +85,12 @@ class sparse_table {
 
   // Size is the maximum number of values the hash table will hold.
   // Overfilling the table could put it into an infinite loop.
-  sparse_table(size_t _m, T _empty, KeyHash _key_hash, long space_mult=-1)
+  sparse_table(size_t _m, T _empty, KeyHash _key_hash, long inp_space_mult=-1)
       : empty(_empty),
         empty_key(std::get<0>(empty)),
         key_hash(_key_hash) {
-    if (space_mult == -1) space_mult = 1.1;
+    double space_mult;
+    if (inp_space_mult == -1) space_mult = 1.1;
     m = (size_t)1 << pbbslib::log2_up((size_t)(space_mult * _m));
     mask = m - 1;
     table = pbbslib::new_array_no_init<T>(m);
