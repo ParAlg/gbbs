32d8884662cc95ca8b824afa5c70e02eddabe514
Laxman Dhulipala <laxmandhulipala@gmail.com>
2019-12-15 20:45:18 -0500

    changes

---
+++ utils/random_reorder.C
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <cmath>
+
+#include "ligra/ligra.h"
+#include "pbbslib/random_shuffle.h"
+#include "ligra/bridge.h"
+#include "pbbslib/strings/string_basics.h"
+
+template <class Graph>
+void randomReorder(Graph& GA, std::string& outfile) {
+  using W = typename Graph::weight_type;
+  size_t n = GA.n;
+  size_t m = GA.m;
+  auto perm = pbbs::random_permutation<uintE>(n);
+
+  auto edges = pbbs::sequence<uintE>(m);
+  auto offs = pbbs::sequence<uintT>(n);
+  parallel_for(0, n, [&] (size_t i) {
+    offs[perm[i]] = GA.get_vertex(i).getOutDegree();
+  });
+  size_t tot = pbbslib::scan_add_inplace(offs.slice());
+  cout << "m = " << m << " tot = " << tot << endl;
+
+  parallel_for(0, n, [&] (size_t i) {
+    size_t off = offs[perm[i]];
+    size_t next_off = (perm[i] == (n-1)) ? m : offs[perm[i]+1];
+    auto map_f = [&] (const uintE& src, const uintE& ngh, const W& wgh) {
+      uintE ngh_perm = perm[ngh];
+      edges[off++] = ngh_perm;
+    };
+    GA.get_vertex(i).mapOutNgh(i, map_f, false);
+    std::sort(edges.begin()+off, edges.begin()+next_off, std::less<uintE>());
+  });
+
+  std::ofstream file (outfile, std::ios::out | std::ios::binary);
+  if (!file.is_open()) {
+    std::cout << "Unable to open file: " << outfile << std::endl;
+    exit(0);
+  }
+  file << "AdjacencyGraph" << endl;
+  file << n << endl;
+  file << m << endl;
+  auto off_chars = pbbslib::sequence_to_string(offs);
+  auto edges_chars = pbbslib::sequence_to_string(edges);
+  file.write(off_chars.begin(), off_chars.size());
+  file.write(edges_chars.begin(), edges_chars.size());
+
+  file.close();
+  cout << "Done" << endl;
+}
+
+template <class Graph>
+double Reorderer(Graph& GA, commandLine P) {
+  auto outfile = P.getOptionValue("-of", "/ssd0/graphs/bench_experiments/out.adj");
+  randomReorder(GA, outfile);
+  exit(0);
+  return 1.0;
+}
+
+generate_main(Reorderer, false);
---
+++ utils/to_edge_list.C
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <cmath>
+
+#include "ligra/ligra.h"
+#include "pbbslib/random_shuffle.h"
+#include "ligra/bridge.h"
+#include "pbbslib/strings/string_basics.h"
+
+/* Format:
+ * Emits a lexicographically sorted list of edges. The format takes (u,v)
+ * and writes:
+ * "u v\n" */
+template <class Graph>
+void print_edge_list(Graph& GA, std::string& outfile, bool direct_sym, bool multistep_header) {
+  using W = typename Graph::weight_type;
+  size_t n = GA.n;
+  size_t m = GA.m;
+
+  auto edges = pbbs::sequence<std::tuple<uintE, uintE>>(m);
+
+  auto offs = pbbs::sequence<size_t>(n);
+  parallel_for(0, n, [&] (size_t i) {
+    size_t ctr = 0;
+    if (direct_sym) {
+      auto f = [&] (const uintE& u, const uintE& v, const W& wgh) {
+        if (u < v) ctr++;
+      };
+      GA.get_vertex(i).mapOutNgh(i, f, false);
+    } else {
+      ctr = GA.get_vertex(i).getOutDegree();
+    }
+    offs[i] = ctr;
+  });
+  size_t m_out = pbbslib::scan_add_inplace(offs.slice());
+
+  parallel_for(0, n, [&] (size_t i) {
+    size_t off = offs[i];
+    size_t ctr = 0;
+    auto map_f = [&] (const uintE& u, const uintE& v, const W& wgh) {
+      if (direct_sym) {
+        if (u < v) {
+          edges[off + ctr] = std::make_tuple(u, v);
+          ctr++;
+        }
+      } else {
+        edges[off + ctr] = std::make_tuple(u, v);
+        ctr++;
+      }
+    };
+    GA.get_vertex(i).mapOutNgh(i, map_f, false);
+  });
+
+  std::ofstream file (outfile, std::ios::out | std::ios::binary);
+  if (!file.is_open()) {
+    std::cout << "Unable to open file: " << outfile << std::endl;
+    exit(0);
+  }
+  if (multistep_header) {
+    file << n << " " << m_out << std::endl;
+  }
+  auto edges_chars = pbbslib::sequence_to_string(edges);
+  file.write(edges_chars.begin(), edges_chars.size());
+
+  file.close();
+  cout << "Done" << endl;
+}
+
+/* Format:
+ * Emits a lexicographically sorted list of edges.
+ *  n m              # header (once)
+ * "u v 1\n"         # per edge*/
+template <class Graph>
+void print_edge_list_matrixmarket(Graph& GA, std::string& outfile) {
+  using W = typename Graph::weight_type;
+  size_t n = GA.n;
+  size_t m = GA.m;
+
+  auto edges = pbbs::sequence<std::tuple<uintE, uintE, uintE>>(m);
+
+  auto offs = pbbs::sequence<size_t>(n);
+  parallel_for(0, n, [&] (size_t i) {
+    size_t ctr = 0;
+      auto f = [&] (const uintE& u, const uintE& v, const W& wgh) {
+        if (u < v) ctr++;
+      };
+      GA.get_vertex(i).mapOutNgh(i, f, false);
+    offs[i] = ctr;
+  });
+  size_t m_out = pbbslib::scan_add_inplace(offs.slice());
+
+  parallel_for(0, n, [&] (size_t i) {
+    size_t off = offs[i];
+    size_t ctr = 0;
+    auto map_f = [&] (const uintE& u, const uintE& v, const W& wgh) {
+      if (u < v) {
+        edges[off + ctr] = std::make_tuple(u, v, 1);
+        ctr++;
+      }
+    };
+    GA.get_vertex(i).mapOutNgh(i, map_f, false);
+  });
+
+  std::ofstream file (outfile, std::ios::out | std::ios::binary);
+  if (!file.is_open()) {
+    std::cout << "Unable to open file: " << outfile << std::endl;
+    exit(0);
+  }
+  file << n << " " << m_out << std::endl;
+  auto edges_chars = pbbslib::sequence_to_string(edges);
+  file.write(edges_chars.begin(), edges_chars.size());
+
+  file.close();
+  cout << "Done" << endl;
+}
+
+template <class Graph>
+double Reorderer(Graph& GA, commandLine P) {
+  auto outfile = P.getOptionValue("-of", "");
+  auto direct_sym = P.getOptionValue("-direct_sym"); /* only emit { (u,v) | u < v } */
+  auto multistep_header = P.getOptionValue("-multistep_header"); /* only emit { (u,v) | u < v } */
+  auto matrixmarket_format = P.getOptionValue("-matrixmarket_format"); /* only emit { (u,v) | u < v } */
+  if (matrixmarket_format) {
+    print_edge_list_matrixmarket(GA, outfile);
+  } else {
+    print_edge_list(GA, outfile, direct_sym, multistep_header);
+  }
+  exit(0);
+  return 1.0;
+}
+
+generate_main(Reorderer, false);
