87d7a3e74fdc63f0c70210a0920ceacca260e6b5
Tom Tseng <tom.hm.tseng@gmail.com>
2020-01-05 00:31:13 -0800

    utils: Overload ASSERT() to not need a message
    Sometimes an assertion condition is self-explanatory and doesn't need an
    additional message. This overloads ASSERT() so that it can take either
    the form ASSERT(condition, message) or ASSERT(condition).

--- utils/assert.h
+++ utils/assert.h
@@ -4,7 +4,33 @@
 #include <exception>
 #include <iostream>
 
-#define ASSERT(condition, message) \
+#include <boost/preprocessor/facilities/overload.hpp>
+
+// Asserts on a condition, printing an error and terminating if the condition is
+// false.
+//
+// This is overloaded and can take either one or two arguments.
+//
+// Arguments
+// ---------
+//   condition: bool
+//     The condition on which to assert.
+//   (optional) message: string
+//     An explanatory message to print out when the condition fails.
+#define ASSERT(...) BOOST_PP_OVERLOAD(__ASSERT, __VA_ARGS__)(__VA_ARGS__)
+
+// Internal. Implementation of ASSERT with one argument.
+#define __ASSERT1(condition) \
+  do { \
+    if (!(condition)) { \
+      std::cerr << __FILE__ << ":" << __LINE__ << ": Failed assertion `"  \
+          #condition "`" << std::endl; \
+      std::terminate(); \
+    } \
+  } while (false)
+
+// Internal. Implementation of ASSERT with two arguments.
+#define __ASSERT2(condition, message) \
   do { \
     if (!(condition)) { \
       std::cerr << __FILE__ << ":" << __LINE__ << ": Failed assertion `"  \
