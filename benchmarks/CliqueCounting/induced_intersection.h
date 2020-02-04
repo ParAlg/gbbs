8d97c995f8d3c5f01cd18fe39f4f5b49d0b0616b
Jessica Shi <jessicashi@gmail.com>
2020-02-03 23:28:36 -0500

    fix intersect?

--- benchmarks/CliqueCounting/induced_intersection.h
+++ benchmarks/CliqueCounting/induced_intersection.h
@@ -27,7 +27,7 @@ namespace induced_intersection {
 
     auto tmp = sequence<std::tuple<uintE, W>>(num_induced);
     auto pred = [&] (const uintE& src, const uintE& nbhr, const W& wgh) {
-      return induced->intersect[nbhr] == k_idx;
+      return (induced->intersect[nbhr] == k_idx);
     };
 
     if (k_idx + 1 == k) {
