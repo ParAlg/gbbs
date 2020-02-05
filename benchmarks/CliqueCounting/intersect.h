bad1a8bf89ebeb7cdf82da775cdaed48c160c126
Jessica Shi <jessicashi@gmail.com>
2020-02-05 17:12:04 -0500

    peel fix

--- benchmarks/CliqueCounting/intersect.h
+++ benchmarks/CliqueCounting/intersect.h
@@ -298,14 +298,9 @@ struct HybridSpace_lw {
     for (size_t j=0; j < nn; j++) { induced_degs[j] = 0; }
   
     //if (k > 2) {
-    //num_induced[0] = nn;
-    //  for (size_t  j=0; j < nn; j++) { induced[j] = j; }
+    num_induced[0] = nn;
+    for (size_t  j=0; j < nn; j++) { induced[j] = j; }
     //}
-    size_t j_idx = 0;
-    size_t j_idx_idx = 0;
-    auto map_j_f = [&] (const uintE& src, const uintE& v, const W& wgh) { if(f(v)) { induced[j_idx_idx] = j_idx; j_idx_idx++; } j_idx++;};
-    DG.get_vertex(i).mapOutNgh(i, map_j_f, false);
-    num_induced[0] = j_idx_idx;
 
     size_t j = 0;
     auto map_f = [&] (const uintE& src, const uintE& v, const W& wgh) {
