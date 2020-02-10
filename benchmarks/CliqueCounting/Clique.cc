eab6be940699fe1e2d48838f967678eef6c57d96
Laxman Dhulipala <laxmandhulipala@gmail.com>
2020-02-10 18:56:35 -0500

    Revert "fix commit, switch from sparse_table<uintE, bool> to <uintE, pbbs::empty>"
    This reverts commit 2a739ab4169bf15830f49831b418e601e030b9c7.

--- benchmarks/CliqueCounting/Clique.cc
+++ benchmarks/CliqueCounting/Clique.cc
@@ -72,7 +72,7 @@ double AppKCore_runner(Graph& GA, commandLine P) {
   assert(P.getOption("-s"));
 
   timer t; t.start();
-  size_t count = Clique(GA, k, order, epsilon, space, label, filter, use_base, recursive_level, par_serial, P);
+  size_t count = Clique(GA, k, order, epsilon, space, label, filter, use_base, recursive_level, par_serial);
   double tt = t.stop();
   std::cout << "count: " << count << std::endl;
   std::cout << "### Running Time: " << tt << std::endl;
