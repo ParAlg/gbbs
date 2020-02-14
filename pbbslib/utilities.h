7291018008c6dc4b42d6d40d1cd952aa53d9f840
Tom Tseng <tom.hm.tseng@gmail.com>
2020-02-14 18:31:46 -0500

    utilities: add cc file

--- pbbslib/utilities.h
+++ pbbslib/utilities.h
@@ -81,55 +81,17 @@ namespace pbbs {
   }
 
   // a 32-bit hash function
-  inline uint32_t hash32(uint32_t a) {
-    a = (a+0x7ed55d16) + (a<<12);
-    a = (a^0xc761c23c) ^ (a>>19);
-    a = (a+0x165667b1) + (a<<5);
-    a = (a+0xd3a2646c) ^ (a<<9);
-    a = (a+0xfd7046c5) + (a<<3);
-    a = (a^0xb55a4f09) ^ (a>>16);
-    return a;
-  }
+  uint32_t hash32(uint32_t a);
 
-  inline uint32_t hash32_2(uint32_t a) {
-    uint32_t z = (a + 0x6D2B79F5UL);
-    z = (z ^ (z >> 15)) * (z | 1UL);
-    z ^= z + (z ^ (z >> 7)) * (z | 61UL);
-    return z ^ (z >> 14);
-  }
+  uint32_t hash32_2(uint32_t a);
 
-  inline uint32_t hash32_3(uint32_t a) {
-      uint32_t z = a + 0x9e3779b9;
-      z ^= z >> 15; // 16 for murmur3
-      z *= 0x85ebca6b;
-      z ^= z >> 13;
-      z *= 0xc2b2ae3d; // 0xc2b2ae35 for murmur3
-      return z ^= z >> 16;
-  }
+  uint32_t hash32_3(uint32_t a);
 
-
-  // from numerical recipes
-  inline uint64_t hash64(uint64_t u )
-  {
-    uint64_t v = u * 3935559000370003845ul + 2691343689449507681ul;
-    v ^= v >> 21;
-    v ^= v << 37;
-    v ^= v >>  4;
-    v *= 4768777513237032717ul;
-    v ^= v << 20;
-    v ^= v >> 41;
-    v ^= v <<  5;
-    return v;
-  }
+  uint64_t hash64(uint64_t u);
 
   // a slightly cheaper, but possibly not as good version
   // based on splitmix64
-  inline uint64_t hash64_2(uint64_t x) {
-    x = (x ^ (x >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
-    x = (x ^ (x >> 27)) * UINT64_C(0x94d049bb133111eb);
-    x = x ^ (x >> 31);
-    return x;
-  }
+  uint64_t hash64_2(uint64_t x);
 
   // Does not initialize the array
   template<typename E>
@@ -160,9 +122,7 @@ namespace pbbs {
     return r;
   }
 
-  inline void free_array(void* a) {
-    my_free(a);
-  }
+  void free_array(void* a);
 
   // Destructs in parallel
   template<typename E>
@@ -318,23 +278,8 @@ namespace pbbs {
     return a;
   }
 
-  inline size_t granularity(size_t n) {
-    return (n > 100) ? ceil(pow(n,0.5)) : 100;
-  }
+  size_t granularity(size_t n);
 
-  inline void assert_str(int cond, std::string s) {
-    if (!cond) {std::cout << "PBBS assert error: " << s << std::endl;}
-  }
+  void assert_str(int cond, std::string s);
 
 }
-
-#ifdef USEMALLOC
-inline void* my_alloc(size_t i) {return malloc(i);}
-inline void my_free(void* p) {free(p);}
-#else
-#include "alloc.h"
-inline void* my_alloc(size_t i) {return my_mem_pool.alloc(i);}
-inline void my_free(void* p) {my_mem_pool.afree(p);}
-#endif
-
-
