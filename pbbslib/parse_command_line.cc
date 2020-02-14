cadf284bce09b51e428335fe271daa3103218a71
Tom Tseng <tom.hm.tseng@gmail.com>
2020-02-14 18:48:04 -0500

    parse_command_line: add cc file

---
+++ pbbslib/parse_command_line.cc
@@ -0,0 +1,84 @@
+#include "parse_command_line.h"
+
+commandLine::commandLine(int _c, char** _v, std::string _cl)
+  : argc(_c), argv(_v), comLine(std::move(_cl)) {
+    if (getOption("-h") || getOption("-help"))
+      badArgument();
+  }
+
+commandLine::commandLine(int _c, char** _v)
+  : argc(_c), argv(_v), comLine("bad arguments") { }
+
+void commandLine::badArgument() {
+  std::cout << "usage: " << argv[0] << " " << comLine << std::endl;
+  exit(0);
+}
+
+char* commandLine::getArgument(int i) {
+  if (argc < 2+i) badArgument();
+  return argv[argc-1-i];
+}
+
+std::pair<char*,char*> commandLine::IOFileNames() {
+  if (argc < 3) badArgument();
+  return std::pair<char*,char*>(argv[argc-2],argv[argc-1]);
+}
+
+std::pair<size_t,char*> commandLine::sizeAndFileName() {
+  if (argc < 3) badArgument();
+  return std::pair<size_t,char*>(std::atoi(argv[argc-2]),(char*) argv[argc-1]);
+}
+
+bool commandLine::getOption(const std::string& option) {
+  for (int i = 1; i < argc; i++)
+    if ((std::string) argv[i] == option) return true;
+  return false;
+}
+
+char* commandLine::getOptionValue(const std::string& option) {
+  for (int i = 1; i < argc-1; i++)
+    if ((std::string) argv[i] == option) return argv[i+1];
+  return NULL;
+}
+
+std::string commandLine::getOptionValue(
+    const std::string& option, const std::string& defaultValue) {
+  for (int i = 1; i < argc-1; i++)
+    if ((std::string) argv[i] == option) return (std::string) argv[i+1];
+  return defaultValue;
+}
+
+long
+commandLine::getOptionLongValue(const std::string& option, long defaultValue) {
+  for (int i = 1; i < argc-1; i++)
+    if ((std::string) argv[i] == option) {
+      long r = atol(argv[i+1]);
+      if (r < 0) badArgument();
+      return r;
+    }
+  return defaultValue;
+}
+
+int
+commandLine::getOptionIntValue(const std::string& option, int defaultValue) {
+  for (int i = 1; i < argc-1; i++)
+    if ((std::string) argv[i] == option) {
+      int r = atoi(argv[i+1]);
+      if (r < 0) badArgument();
+      return r;
+    }
+  return defaultValue;
+}
+
+double commandLine::getOptionDoubleValue(
+    const std::string& option, double defaultValue) {
+  for (int i = 1; i < argc-1; i++)
+    if ((std::string) argv[i] == option) {
+      double val;
+      if (sscanf(argv[i+1], "%lf",  &val) == EOF) {
+        badArgument();
+      }
+      return val;
+    }
+  return defaultValue;
+}
--- pbbslib/parse_command_line.h
+++ pbbslib/parse_command_line.h
@@ -31,87 +31,33 @@ struct commandLine {
   int argc;
   char** argv;
   std::string comLine;
-  commandLine(int _c, char** _v, std::string _cl)
-    : argc(_c), argv(_v), comLine(_cl) {
-      if (getOption("-h") || getOption("-help"))
-	badArgument();
-    }
+  commandLine(int _c, char** _v, std::string _cl);
 
-  commandLine(int _c, char** _v)
-    : argc(_c), argv(_v), comLine("bad arguments") { }
+  commandLine(int _c, char** _v);
 
-  void badArgument() {
-    std::cout << "usage: " << argv[0] << " " << comLine << std::endl;
-    exit(0);
-  }
+  void badArgument();
 
   // get an argument
   // i is indexed from the last argument = 0, second to last indexed 1, ..
-  char* getArgument(int i) {
-    if (argc < 2+i) badArgument();
-    return argv[argc-1-i];
-  }
+  char* getArgument(int i);
 
   // looks for two filenames
-  std::pair<char*,char*> IOFileNames() {
-    if (argc < 3) badArgument();
-    return std::pair<char*,char*>(argv[argc-2],argv[argc-1]);
-  }
+  std::pair<char*,char*> IOFileNames();
 
-  std::pair<size_t,char*> sizeAndFileName() {
-    if (argc < 3) badArgument();
-    return std::pair<size_t,char*>(std::atoi(argv[argc-2]),(char*) argv[argc-1]);
-  }
+  std::pair<size_t,char*> sizeAndFileName();
 
-  bool getOption(std::string option) {
-    for (int i = 1; i < argc; i++)
-      if ((std::string) argv[i] == option) return true;
-    return false;
-  }
+  bool getOption(const std::string& option);
 
-  char* getOptionValue(std::string option) {
-    for (int i = 1; i < argc-1; i++)
-      if ((std::string) argv[i] == option) return argv[i+1];
-    return NULL;
-  }
+  char* getOptionValue(const std::string& option);
 
-  std::string getOptionValue(std::string option, std::string defaultValue) {
-    for (int i = 1; i < argc-1; i++)
-      if ((std::string) argv[i] == option) return (std::string) argv[i+1];
-    return defaultValue;
-  }
+  std::string
+  getOptionValue(const std::string& option, const std::string& defaultValue);
 
-  long getOptionLongValue(std::string option, long defaultValue) {
-    for (int i = 1; i < argc-1; i++)
-      if ((std::string) argv[i] == option) {
-	long r = atol(argv[i+1]);
-	if (r < 0) badArgument();
-	return r;
-      }
-    return defaultValue;
-  }
+  long getOptionLongValue(const std::string& option, long defaultValue);
 
-  int getOptionIntValue(std::string option, int defaultValue) {
-    for (int i = 1; i < argc-1; i++)
-      if ((std::string) argv[i] == option) {
-	int r = atoi(argv[i+1]);
-	if (r < 0) badArgument();
-	return r;
-      }
-    return defaultValue;
-  }
+  int getOptionIntValue(const std::string& option, int defaultValue);
 
-  double getOptionDoubleValue(std::string option, double defaultValue) {
-    for (int i = 1; i < argc-1; i++)
-      if ((std::string) argv[i] == option) {
-	double val;
-	if (sscanf(argv[i+1], "%lf",  &val) == EOF) {
-	  badArgument();
-	}
-	return val;
-      }
-    return defaultValue;
-  }
+  double getOptionDoubleValue(const std::string& option, double defaultValue);
 
 };
 
