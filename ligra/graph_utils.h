21d21c674f497b4d03ea7f8105053da319b22a40
Tom Tseng <tom.hm.tseng@gmail.com>
2020-02-14 21:06:53 -0500

    graph_utils: remove (unused)

--- ligra/graph_utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/* Represents per-vertex information necessary to extract the neighbors of a
- * vertex out of Compressed Sparse Row (CSR) */
-struct vertex_data {
-  /* offset into the edges in CSR */
-  size_t offset;
-};
