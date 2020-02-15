c0f4f1d6f4f5f16ad996d73a83b993174b19b9d0
Tom Tseng <tom.hm.tseng@gmail.com>
2020-02-14 19:06:43 -0500

    string_basics: add cc

--- pbbslib/strings/string_basics.h
+++ pbbslib/strings/string_basics.h
@@ -59,24 +59,6 @@ namespace pbbs {
   // ********************************
   // Code Follows
   // ********************************
-  inline sequence<char> char_seq_from_file(std::string filename,
-					   size_t start, size_t end) {
-    std::ifstream file (filename, std::ios::in | std::ios::binary | std::ios::ate);
-    if (!file.is_open()) {
-      std::cout << "Unable to open file: " << filename << std::endl;
-      exit(1);
-    }
-    size_t length = file.tellg();
-    start = std::min(start,length);
-    if (end == 0) end = length;
-    else end = std::min(end,length);
-    size_t n = end - start;
-    file.seekg (start, std::ios::beg);
-    char* bytes = new_array<char>(n+1);
-    file.read (bytes,n);
-    file.close();
-    return sequence<char>(bytes,n);
-  }
 
   template <class CharSeq>
   int char_seq_to_file(CharSeq const &S, char const *fileName) {
