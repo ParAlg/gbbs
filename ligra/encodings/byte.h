a88c07d3ce4679038b4780091783f8b49c5a915a
Tom Tseng <tom.hm.tseng@gmail.com>
2020-02-14 22:10:58 -0500

    byte: add cc

--- ligra/encodings/byte.h
+++ ligra/encodings/byte.h
@@ -37,7 +37,7 @@
 
 namespace byte {
 
-inline size_t get_virtual_degree(uintE d, uchar* nghArr) { return d; }
+size_t get_virtual_degree(uintE d, uchar* nghArr);
 
 // Read an empty weight (noop)
 template <class W, typename std::enable_if<std::is_same<W, pbbslib::empty>::value,
@@ -77,41 +77,12 @@ inline float eatWeight(uchar*& start) {
   return *float_start;
 }
 
-inline intE eatFirstEdge(uchar*& start, uintE source) {
-  uchar fb = *start++;
-  intE edgeRead = (fb & 0x3f);
-  if (LAST_BIT_SET(fb)) {
-    int shiftAmount = 6;
-    while (1) {
-      uchar b = *start;
-      edgeRead |= ((b & 0x7f) << shiftAmount);
-      start++;
-      if (LAST_BIT_SET(b))
-        shiftAmount += EDGE_SIZE_PER_BYTE;
-      else
-        break;
-    }
-  }
-  return (fb & 0x40) ? source - edgeRead : source + edgeRead;
-}
+intE eatFirstEdge(uchar*& start, uintE source);
 
 /*
   Reads any edge of an out-edge list after the first edge.
 */
-inline uintE eatEdge(uchar*& start) {
-  uintE edgeRead = 0;
-  int shiftAmount = 0;
-
-  while (1) {
-    uchar b = *start++;
-    edgeRead += ((b & 0x7f) << shiftAmount);
-    if (LAST_BIT_SET(b))
-      shiftAmount += EDGE_SIZE_PER_BYTE;
-    else
-      break;
-  }
-  return edgeRead;
-}
+uintE eatEdge(uchar*& start);
 
 template <class W, class T>
 inline void decode(T t, uchar* edgeStart, const uintE& source,
@@ -174,39 +145,8 @@ inline E map_reduce(uchar* edge_start, const uintE& source, const uintT& degree,
 /*
   Compresses the first edge, writing target-source and a sign bit.
 */
-inline long compressFirstEdge(uchar* start, long offset, long source,
-                              long target) {
-  long diff = target - source;
-  long preCompress = diff;
-  int bytesUsed = 0;
-  uchar firstByte = 0;
-  uintE toCompress = std::abs(preCompress);
-  firstByte = toCompress & 0x3f;  // 0011|1111
-  if (preCompress < 0) {
-    firstByte |= 0x40;
-  }
-  toCompress = toCompress >> 6;
-  if (toCompress > 0) {
-    firstByte |= 0x80;
-  }
-  start[offset] = firstByte;
-  offset++;
-
-  uchar curByte = toCompress & 0x7f;
-  while ((curByte > 0) || (toCompress > 0)) {
-    bytesUsed++;
-    uchar toWrite = curByte;
-    toCompress = toCompress >> 7;
-    // Check to see if there's any bits left to represent
-    curByte = toCompress & 0x7f;
-    if (toCompress > 0) {
-      toWrite |= 0x80;
-    }
-    start[offset] = toWrite;
-    offset++;
-  }
-  return offset;
-}
+long compressFirstEdge(uchar* start, long offset, long source,
+                              long target);
 
 template <class W, typename std::enable_if<std::is_same<W, pbbslib::empty>::value,
                                            int>::type = 0>
@@ -228,23 +168,7 @@ inline long compressWeight(uchar* start, long offset, W weight) {
   return offset + sizeof(float);
 }
 
-inline long compressEdge(uchar* start, long curOffset, uintE diff) {
-  uchar curByte = diff & 0x7f;
-  int bytesUsed = 0;
-  while ((curByte > 0) || (diff > 0)) {
-    bytesUsed++;
-    uchar toWrite = curByte;
-    diff = diff >> 7;
-    // Check to see if there's any bits left to represent
-    curByte = diff & 0x7f;
-    if (diff > 0) {
-      toWrite |= 0x80;
-    }
-    start[curOffset] = toWrite;
-    curOffset++;
-  }
-  return curOffset;
-}
+long compressEdge(uchar* start, long curOffset, uintE diff);
 
 template <class W, class P>
 inline size_t pack(P& pred, uchar* edge_start, const uintE& source,
