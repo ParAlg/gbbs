86ceab500a685da1709c7eca14ef61814dc227e3
Tom Tseng <tom.hm.tseng@gmail.com>
2020-03-05 17:11:56 -0500

    graph_test_utils: Add option to sort neighbors in making test graphs

--- ligra/graph_test_utils.cc
+++ ligra/graph_test_utils.cc
@@ -2,14 +2,18 @@
 
 #include <tuple>
 
+#include "pbbslib/sample_sort.h"
+
 namespace graph_test {
 
 symmetric_graph<symmetric_vertex, pbbslib::empty> MakeUnweightedSymmetricGraph(
     const uintE num_vertices,
-    const std::unordered_set<UndirectedEdge>& edges) {
+    const std::unordered_set<UndirectedEdge>& edges,
+    const ShouldSortNeighbors should_sort_neighbors) {
+  using Edge = std::tuple<uintE, uintE, pbbslib::empty>;
   constexpr pbbs::empty weight{};
-  pbbs::sequence<std::tuple<uintE, uintE, pbbslib::empty>> edge_sequence(
-      edges.size() * 2);
+
+  pbbs::sequence<Edge> edge_sequence(edges.size() * 2);
   auto edges_it{edges.cbegin()};
   for (size_t i = 0; i < edges.size(); i++) {
     edge_sequence[2 * i] =
@@ -24,7 +28,22 @@ symmetric_graph<symmetric_vertex, pbbslib::empty> MakeUnweightedSymmetricGraph(
           weight);
     ++edges_it;
   }
-  return sym_graph_from_edges(edge_sequence, num_vertices);
+
+  switch (should_sort_neighbors) {
+    case ShouldSortNeighbors::kYes: {
+      pbbs::sample_sort_inplace(
+          edge_sequence.slice(),
+          [](const Edge& left, const Edge& right) {
+            return std::tie(std::get<0>(left), std::get<1>(left))
+              < std::tie(std::get<0>(right), std::get<1>(right));
+          });
+      const bool edges_are_sorted{true};
+      return sym_graph_from_edges(edge_sequence, num_vertices, edges_are_sorted);
+    }
+    case ShouldSortNeighbors::kNo: {
+      return sym_graph_from_edges(edge_sequence, num_vertices);
+    }
+  }
 }
 
 }  // namespace graph_test
