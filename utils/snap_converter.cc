568022cea908faf4e0f0e482e114f545ad78fec5
Tom Tseng <tomtseng@users.noreply.github.com>
2020-04-06 04:35:40 -0700

    utils: Add SNAP converter (#28)
    Add a converter that can convert edge-list graph files to adjacency graph files.
    
    ## Testing
    - Unit tests
    - Output of converting the symmetric SNAP graph [wiki-Vote](https://snap.stanford.edu/data/wiki-Vote.html) is the same as that of [`jshun/ligra/utils/SNAPtoAdj`](https://github.com/jshun/ligra/blob/9e1f3f2cce2d341adf2939fdca7f8a9aaf8e95d4/utils/SNAPtoAdj.C)
    - Output of converting a short hand-written list of directed edges is the same as that of `jshun/ligra/utils/SNAPtoAdj` up to neighbor ordering (`SNAPtoAdj` does not sort each neighbor list whereas the converter in this PR does)
    - Not much testing on weighted graphs since SNAP doesn't have any clean weighted graphs, but the output of converting a short hand-written list of weighted edges looked correct.
    
    __TODO__: Add makefile command (I think I'm going to delegate this to @jeshi96, she can probably write it much faster than I can)

---
+++ utils/snap_converter.cc
@@ -0,0 +1,100 @@
+// Converts a graph in edge-list format to the adjacency graph format that
+// GBBS uses. In particular, graph files downloaded from the SNAP dataset
+// collection (http://snap.stanford.edu/data/index.html) are often in edge-list
+// format and may be converted using this tool.
+//
+// Pass the `--help` flag to see usage directions.
+//
+// The contents of an unweighted edge-list file should be as follows:
+//     # There can be comments at the top of the file as long as each line of
+//     # the comment starts with '#'.
+//     <edge 1 first endpoint> <edge 1 second endpoint>
+//     <edge 2 first endpoint> <edge 2 second endpoint>
+//     <edge 3 first endpoint> <edge 3 second endpoint>
+//     ...
+//     <edge m first endpoint> <edge m second endpoint>
+//
+// The contents of a weighted edge-list file should be as follows:
+//     # There can be comments at the top of the file as long as each line of
+//     # the comment starts with '#'.
+//     <edge 1 first endpoint> <edge 1 second endpoint> <edge 1 weight>
+//     <edge 2 first endpoint> <edge 2 second endpoint> <edge 2 weight>
+//     <edge 3 first endpoint> <edge 3 second endpoint> <edge 3 weight>
+//     ...
+//     <edge m first endpoint> <edge m second endpoint> <edge m weight>
+#include <exception>
+#include <vector>
+
+#include "ligra/graph_io.h"
+#include "ligra/parse_command_line.h"
+
+namespace {
+
+// Convert `edge_list` to a graph and write it to `output_file`.
+template <typename Weight>
+void WriteEdgeListAsGraph(
+    const char* output_file,
+    const std::vector<gbbs_io::Edge<Weight>>& edge_list,
+    bool is_symmetric_graph) {
+  if (is_symmetric_graph) {
+    auto graph{gbbs_io::edge_list_to_symmetric_graph(edge_list)};
+    gbbs_io::write_graph_to_file(output_file, graph);
+  } else {
+    auto graph{gbbs_io::edge_list_to_asymmetric_graph(edge_list)};
+    gbbs_io::write_graph_to_file(output_file, graph);
+  }
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  const std::string kCommandLineHelpString{
+    "Usage: ./snap_converter [-s] [-w] -i <input file> -o <output file>\n"
+    "\n"
+    "Converts a graph in edge-list format to the adjacency graph format that\n"
+    "GBBS uses. In particular, graph files downloaded from the SNAP dataset\n"
+    "collection (http://snap.stanford.edu/data/index.html) are often in\n"
+    "edge-list format and may be converted using this tool.\n"
+    "\n"
+    "Arguments:\n"
+    "  -i <filename>: Path to the input edge-list file.\n"
+    "  -o <filename>: Path to the output adjacency graph file.\n"
+    "Optional arguments:\n"
+    "  -s: Treat the edges as a list of undirected edges and create a\n"
+    "      symmetric graph. (Without this flag, the edges are treated as a\n"
+    "      list of directed edges.)\n"
+    "  -w: Use this flag if the edge list is weighted.\n"
+  };
+  const std::string kInputFlag{"-i"};
+  const std::string kOutputFlag{"-o"};
+
+  const commandLine parameters{argc, argv, kCommandLineHelpString};
+  const char* const input_file{parameters.getOptionValue(kInputFlag)};
+  const char* const output_file{parameters.getOptionValue(kOutputFlag)};
+  const bool is_symmetric_graph{parameters.getOption("-s")};
+  const bool weighted{parameters.getOption("-w")};
+
+  if (argc < 2 ||
+      std::string(argv[1]) == "-h" ||
+      std::string(argv[1]) == "--help") {
+    std::cout << kCommandLineHelpString << '\n';
+    return 0;
+  }
+
+  if (input_file == nullptr || output_file == nullptr) {
+    std::cerr << "ERROR: Please specify the input SNAP file with the '" <<
+      kInputFlag << "' flag and the output file with the '" << kOutputFlag <<
+      "' flag.\n";
+    std::terminate();
+  }
+
+  if (weighted) {
+    const auto edge_list{gbbs_io::read_weighted_edge_list(input_file)};
+    WriteEdgeListAsGraph(output_file, edge_list, is_symmetric_graph);
+  } else {
+    const auto edge_list{gbbs_io::read_unweighted_edge_list(input_file)};
+    WriteEdgeListAsGraph(output_file, edge_list, is_symmetric_graph);
+  }
+
+  return 0;
+}
