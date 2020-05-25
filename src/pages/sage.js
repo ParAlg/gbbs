ce0590156e6d3bd2c06a0d484cc5961e7d7f1507
Laxman Dhulipala <laxmandhulipala@gmail.com>
2020-05-25 16:01:49 -0700

    sage redirect

---
+++ src/pages/sage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import  { Redirect } from 'react-router-dom'
+import Layout from '@theme/Layout';
+
+function Sage() {
+  return <Redirect to='docs/sage'  />
+}
+
+export default Sage;
