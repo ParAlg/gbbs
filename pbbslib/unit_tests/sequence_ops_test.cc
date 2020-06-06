0cd2a0ede4d2d55d39b5c70750f01dc69718b791
Tom Tseng <tom.hm.tseng@gmail.com>
2020-06-05 23:58:03 -0700

    more universal refs where needed

--- pbbslib/unit_tests/sequence_ops_test.cc
+++ pbbslib/unit_tests/sequence_ops_test.cc
@@ -24,18 +24,11 @@ TEST(PackOut, PackIntoSlice) {
   EXPECT_THAT(even_numbers, ElementsAre(0, 10, 12, 14, 0));
 }
 
-TEST(FilterOut, FilterIntoSequence) {
+TEST(PackOut, PackIntoReverseSlice) {
   pbbs::sequence<int32_t> numbers{5, [&](const size_t i) { return i + 10; }};
-  constexpr auto is_even{[](const int32_t x) { return x % 2 == 0; }};
-  pbbs::sequence<int32_t> even_numbers(3, 0);
-  pbbs::filter_out(numbers, even_numbers, is_even);
-  EXPECT_THAT(even_numbers, ElementsAre(10, 12, 14));
-}
-
-TEST(FilterOut, FilterIntoSlice) {
-  pbbs::sequence<int32_t> numbers{5, [&](const size_t i) { return i + 10; }};
-  constexpr auto is_even{[](const int32_t x) { return x % 2 == 0; }};
+  pbbs::sequence<bool> even_flags{
+    5, [&](const size_t i) { return i % 2 == 0; }};
   pbbs::sequence<int32_t> even_numbers(5, 0);
-  pbbs::filter_out(numbers, even_numbers.slice(1, 4), is_even);
-  EXPECT_THAT(even_numbers, ElementsAre(0, 10, 12, 14, 0));
+  pbbs::pack_out(numbers, even_flags, even_numbers.rslice(1, 4));
+  EXPECT_THAT(even_numbers, ElementsAre(0, 14, 12, 10, 0));
 }
