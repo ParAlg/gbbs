3741dfacda7d43f6287c37f5ed47725a0ea88e14
Tom Tseng <tom.hm.tseng@gmail.com>
2020-07-07 01:32:38 -0700

    similarity_measure: mv internal func header comments to header file

--- benchmarks/SCAN/IndexBased/similarity_measure.h
+++ benchmarks/SCAN/IndexBased/similarity_measure.h
@@ -132,6 +132,8 @@ struct ApproxJaccardSimilarity {
 
 namespace internal {
 
+// Pseudorandomly generate `num_numbers` random normal numbers, each with zero
+// mean and unit variance.
 pbbs::sequence<float>
 RandomNormalNumbers(size_t num_numbers, pbbs::random rng);
 
@@ -142,6 +144,19 @@ DivideRoundingUp(const size_t numerator, const size_t denominator) {
   return (numerator - 1) / denominator + 1;
 }
 
+// Given a sequence of similarities for each undirected edge, construct a
+// sequence of similarities for each directed edge, assuming that
+// similarity(u, v) = similarity(v, u) = similarity({u, v}) for every pair of
+// adjacent vertices u and v.
+//
+// Arguments:
+//   num_directed_edges
+//     The number of directed edges in the graph. (Should be `graph->m` for a
+//     graph `graph`.)
+//   undirectional_similarities
+//     A sequence of similarities for each undirected edge {u, v}. Entries with
+//     NaN as their `.similarity` field are skipped.
+// Given `undirectional_similarities` filled with edge similarities
 pbbs::sequence<EdgeSimilarity> BidirectionalSimilarities(
     const size_t num_directed_edges,
     const pbbs::sequence<EdgeSimilarity>& unidirectional_similarities);
