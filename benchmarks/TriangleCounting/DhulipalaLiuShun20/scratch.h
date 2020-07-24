8ca53d883a8f8ce120a0b6928359a459b4e51aaf
shangdiy <sy543@cornell.edu>
2020-07-24 14:26:06 -0400

    dynamic triangle initialization compile

---
+++ benchmarks/TriangleCounting/DhulipalaLiuShun20/scratch.h
@@ -0,0 +1,9 @@
+            // auto map_f = [&](const uintE& u, const uintE& v, const W& wgh) {
+            //     if(is_low(v_data[v].degree)){flag[]}
+            //     // int mark = 1;
+            //     // if(is_high(v_data[v].degree)){
+            //     //     mark = 2;
+            //     // }
+            //     // flag[u] = flag[u]|mark; // if low, flag is 1, if high is 2, if both is 3
+            // };
+            // mapEdges(map_f, true);
