65a32f6cdc1663956494862cb7dab7247f6734bc
Tom Tseng <tom.hm.tseng@gmail.com>
2020-08-13 03:25:04 +0000

    threshold

--- benchmarks/SCAN/IndexBased/similarity_measure.h
+++ benchmarks/SCAN/IndexBased/similarity_measure.h
@@ -907,7 +907,7 @@ pbbs::sequence<EdgeSimilarity> ApproxCosineSimilarity::AllEdges(
 template <template <typename> class VertexTemplate>
 pbbs::sequence<EdgeSimilarity> ApproxJaccardSimilarity::AllEdges(
     symmetric_graph<VertexTemplate, pbbs::empty>* graph) const {
-  const size_t degree_threshold{static_cast<size_t>(1.5 * num_samples_)};
+  const size_t degree_threshold{static_cast<size_t>(1.0 * num_samples_)};
   return internal::ApproxJaccardEdgeSimilarities(
       graph, num_samples_, degree_threshold, random_seed_);
 }
