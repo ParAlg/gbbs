6da62a5fe010e8ad0b942dcaadabeb87ad672787
Laxman Dhulipala <laxmandhulipala@gmail.com>
2020-12-25 23:00:27 -0500

    missing intersection file

---
+++ gbbs/uncompressed_intersection.h
@@ -0,0 +1,152 @@
+// This code is part of the project "Theoretically Efficient Parallel Graph
+// Algorithms Can Be Fast and Scalable", presented at Symposium on Parallelism
+// in Algorithms and Architectures, 2018.
+// Copyright (c) 2018 Laxman Dhulipala, Guy Blelloch, and Julian Shun
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all  copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include "pbbslib/sequence_ops.h"
+#include "macros.h"
+
+namespace gbbs {
+namespace intersection {
+
+template <template <typename W> class vertex, class W>
+inline size_t intersect(vertex<W>* A, vertex<W>* B, uintE a, uintE b) {
+  uintT i = 0, j = 0, nA = A->getOutDegree(), nB = B->getOutDegree();
+  auto nghA = A->getOutNeighbors();
+  auto nghB = B->getOutNeighbors();
+  size_t ans = 0;
+  while (i < nA && j < nB) {
+    if (std::get<0>(nghA[i]) == std::get<0>(nghB[j]))
+      i++, j++, ans++;
+    else if (std::get<0>(nghA[i]) < std::get<0>(nghB[j]))
+      i++;
+    else
+      j++;
+  }
+  return ans;
+}
+
+template <template <typename W> class vertex, class W, class F>
+inline size_t intersect_f(vertex<W>* A, vertex<W>* B, uintE a, uintE b,
+                          const F& f) {
+  uintT i = 0, j = 0, nA = A->getOutDegree(), nB = B->getOutDegree();
+  auto nghA = A->getOutNeighbors();
+  auto nghB = B->getOutNeighbors();
+  size_t ans = 0;
+  while (i < nA && j < nB) {
+    if (std::get<0>(nghA[i]) == std::get<0>(nghB[j])) {
+      f(a, b, std::get<0>(nghA[i]));
+      i++, j++, ans++;
+    } else if (std::get<0>(nghA[i]) < std::get<0>(nghB[j])) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return ans;
+}
+
+constexpr const size_t _bs_merge_base = 32;
+constexpr const size_t _seq_merge_thresh = 2048;
+
+template <class SeqA, class SeqB, class F>
+size_t seq_merge_full(SeqA& A, SeqB& B, F& f) {
+  using T = typename SeqA::value_type;
+  size_t nA = A.size(), nB = B.size();
+  size_t i = 0, j = 0;
+  size_t ct = 0;
+  while (i < nA && j < nB) {
+    const T& a = A[i];
+    const T& b = B[j];
+    if (a == b) {
+      f(a);
+      i++;
+      j++;
+      ct++;
+    } else if (a < b) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return ct;
+}
+
+template <class SeqA, class SeqB, class F>
+size_t seq_merge(const SeqA& A, const SeqB& B, const F& f) {
+  using T = typename SeqA::value_type;
+  size_t nA = A.size();
+  size_t ct = 0;
+  for (size_t i=0; i < nA; i++) {
+    const T& a = A[i];
+    size_t mB = pbbslib::binary_search(B, a, std::less<T>());
+    if (mB < B.size() && a == B[mB]) {
+      f(a);
+      ct++;
+    }
+  }
+  return ct;
+}
+
+template <class SeqA, class SeqB, class F>
+size_t merge(const SeqA& A, const SeqB& B, const F& f) {
+  using T = typename SeqA::value_type;
+  size_t nA = A.size();
+  size_t nB = B.size();
+  size_t nR = nA + nB;
+  if (nR < _seq_merge_thresh) { // handles (small, small) using linear-merge
+    return intersection::seq_merge_full(A, B, f);
+  } else if (nB < nA) {
+    return intersection::merge(B, A, f);
+  } else if (nA < _bs_merge_base) {
+    return intersection::seq_merge(A, B, f);
+  } else {
+    size_t mA = nA/2;
+    size_t mB = pbbslib::binary_search(B, A[mA], std::less<T>());
+    size_t m_left = 0;
+    size_t m_right = 0;
+    par_do([&] () { m_left = intersection::merge(A.slice(0, mA), B.slice(0, mB), f);},
+     [&] () { m_right = intersection::merge(A.slice(mA, nA), B.slice(mB, nB), f);});
+    return m_left + m_right;
+  }
+}
+
+template <template <typename W> class vertex, class W, class F>
+inline size_t intersect_f_par(vertex<W>* A, vertex<W>* B, uintE a, uintE b,
+                          const F& f) {
+  uintT nA = A->getOutDegree(), nB = B->getOutDegree();
+  uintE* nghA = (uintE*)(A->getOutNeighbors());
+  uintE* nghB = (uintE*)(B->getOutNeighbors());
+
+  // Will not work if W is not pbbslib::empty, should assert.
+  auto seqA = pbbslib::make_sequence<uintE>(nghA, nA);
+  auto seqB = pbbslib::make_sequence<uintE>(nghB, nB);
+
+  auto merge_f = [&] (uintE ngh) {
+    f(a, b, ngh);
+  };
+  return intersection::merge(seqA, seqB, merge_f);
+}
+
+}  // namespace intersection
+}  // namespace gbbs
