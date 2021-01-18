27f79ed7b3cad6945b42445898c96631b535a476
Laxman Dhulipala <laxmandhulipala@gmail.com>
2021-01-18 01:29:07 -0500

    fix lower invariant

--- benchmarks/EdgeOrientation/LayeredDataStructure/LDS.h
+++ benchmarks/EdgeOrientation/LayeredDataStructure/LDS.h
@@ -103,7 +103,9 @@ struct LDS {
     inline bool lower_invariant(const size_t levels_per_group) const {
       if (level == 0) return true;
       uintE lower_group = (level - 1) / levels_per_group;
-      return down[level - 1].size() >= static_cast<size_t>(1 << lower_group);
+      auto up_size = up.size();
+      auto prev_level_size = down[level - 1].size();
+      return (up_size + prev_level_size) >= static_cast<size_t>(1 << lower_group);
     }
 
   };
@@ -139,6 +141,8 @@ struct LDS {
       if (get_level(ngh) == level) {
         L[ngh].up.erase(L[ngh].up.find(u));
         L[ngh].down[level-1].insert(u);
+
+//        Dirty.push(ngh);
       } else if (get_level(ngh) > level) {
         L[ngh].down[level].erase(L[ngh].down[level].find(u));
         L[ngh].down[level - 1].insert(u);
@@ -202,8 +206,11 @@ struct LDS {
         // Move u to level i+1.
         level_increase(u, L);
         Dirty.push(u);  // u might need to move up more levels.
+        std::cout << "(move up) pushing u = " << u << std::endl;
       } else if (!L[u].lower_invariant(levels_per_group)) {
         level_decrease(u, L);
+        Dirty.push(u);  // u might need to move up more levels.
+        std::cout << "(move down) pushing u = " << u << std::endl;
       }
     }
   }
@@ -228,8 +235,7 @@ struct LDS {
     L[u].insert_neighbor(v, l_v);
     L[v].insert_neighbor(u, l_u);
 
-    Dirty.push(u);
-    Dirty.push(v);
+    Dirty.push(u); Dirty.push(v);
     fixup();
     return true;
   }
@@ -246,6 +252,13 @@ struct LDS {
     Dirty.push(u); Dirty.push(v);
     fixup();
     return true;
+  }
+
+  void check_invariants() {
+    for (size_t i=0; i<n; i++) {
+      assert(L[i].upper_invariant(levels_per_group));
+      assert(L[i].lower_invariant(levels_per_group));
+    }
   }
 
   inline uintE group_for_level(uintE level) const {
@@ -269,6 +282,7 @@ inline void RunLDS(Graph& G) {
   }
 
   std::cout << "Finished all insertions!" << std::endl;
+  layers.check_invariants();
 
   for (size_t i = 0; i < 1000; i++) {
     auto map_f = [&](const uintE& u, const uintE& v, const W& wgh) {
@@ -282,6 +296,7 @@ inline void RunLDS(Graph& G) {
   }
 
   std::cout << "Finished all deletions!" << std::endl;
+  layers.check_invariants();
 
   size_t sum_lev = 0;
   for (size_t i=0; i<G.n; i++) {
