5c0cb0131dac646619af56cd604f7776a44bb4f7
Jessica Shi <jessicashi@gmail.com>
2021-01-29 22:17:58 -0500

    update script

--- scripts/test_kcore.py
+++ scripts/test_kcore.py
@@ -28,36 +28,39 @@ def appendToFile(out, filename):
 def main():
   # Configured for Test 1
   program_dir = "../benchmarks/"
-  programs = ["EdgeOrientation/LDS/LDS", "KCore/ApproximateKCore/KCore"]#, "KCore/JulienneDBS17/KCore"]
+  programs = ["EdgeOrientation/LDS/LDS", "KCore/ApproximateKCore/KCore", "KCore/JulienneDBS17/KCore"]
+  program_pres = ["lds", "kcore"]
   is_dynamic = [True, False, False]
-  files = ["dblp_edges", "orkut_edges"]
-  pres = ["dblp", "orkut"]
+  files = ["dblp_edges","orkut_edges"]
+  pres = ["dblp","orkut"]
   empty = "empty_h"
-  stats = "-stats"
-  epss = [0.2, 0.4, 0.8, 1.6, 3.2, 6.4]
-  deltas = [3, 6, 12, 24, 48, 96]
-  batch_sizes = [1000]#[100, 1000, 10000, 100000, 1000000]
+  stats = ""
+  epss = [0.4] #[0.2, 0.4, 0.8, 1.6, 3.2, 6.4]
+  deltas = [3] #[3, 6, 12, 24, 48, 96]
+  batch_sizes = [100, 1000, 10000, 100000, 1000000, 10000000]
   num_workers = [60]#[1, 2, 4, 8, 16, 32, 60]
-  read_dir = "~/"
-  write_dir = ""
+  read_dir = "/home/jeshi/dynamic_graph/"
+  write_dir = "/home/jeshi/dogfood-out/"
   for file_idx, filename in enumerate(files):
     for program_idx, program in enumerate(programs):
       for e in epss:
         for d in deltas:
           for b in batch_sizes:
             for nw in num_workers:
-              out_filename = write_dir + pres[file_idx] + "_" + str(e) + "_" + str(d) + "_" + str(b) + "_" + str(nw) + ".out"
+              out_filename = write_dir + program_pres[program_idx] + "_" + pres[file_idx] + "_" + str(e) + "_" + str(d) + "_" + str(b) + "_" + str(nw) + ".out"
               batch_commands = []
               if is_dynamic[program_idx]:
                 batch_commands = ["-b " + str(b)]
               else:
                 num_lines = sum(1 for line in open(read_dir + filename))
                 batch_commands = ["-num_dynamic_edges " + str(x) for x in range(b, num_lines + 1, b)]
+                if (num_lines % b != 0):
+                  batch_commands.append(num_lines)
               for bc in batch_commands:
                 ss = ("PARLAY_NUM_THREADS=" + str(nw) + " " + program_dir + program + " -s -i"
                 " " + read_dir + filename + " -eps " + str(e) + " "
                 "-delta " + str(d) + " " + bc + " "
-                "-rounds 3 " + stats + " " + read_dir + empty)
+                "-rounds 1 " + stats + " " + read_dir + empty)
                 out = shellGetOutput(ss)
                 appendToFile(out, out_filename)
 
