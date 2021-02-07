18aa62f81406c6175a9a8ff73905d2c3440137aa
Jessica Shi <jessicashi@gmail.com>
2021-02-07 16:35:37 -0500

    test script update

--- scripts/test_kcore.py
+++ scripts/test_kcore.py
@@ -47,31 +47,32 @@ def main():
   epss = [0.4] #[0.2, 0.4, 0.8, 1.6, 3.2, 6.4]
   deltas = [3] #[3, 6, 12, 24, 48, 96]
   batch_sizes = [100]#, 1000, 10000, 100000, 1000000, 10000000]
-  num_workers = [60]#[1, 2, 4, 8, 16, 32, 60]
+  num_workers = [60]#[1, 2, 4, 8, 16, 30, 60]
   read_dir = "/home/jeshi/dynamic_graph/"
   write_dir = "/home/jeshi/dogfood-out/"
   for file_idx, filename in enumerate(files):
+    num_lines = sum(1 for line in open(read_dir + filename))
     for program_idx, program in enumerate(programs):
       for e in epss:
         for d in deltas:
           for b in batch_sizes:
             for nw in num_workers:
+              num_rounds = 4
               time = 0
               out_filename = write_dir + program_pres[program_idx] + "_" + pres[file_idx] + "_" + str(e) + "_" + str(d) + "_" + str(b) + "_" + str(nw) + ".out"
               batch_commands = []
               if is_dynamic[program_idx]:
                 batch_commands = ["-b " + str(b)]
+              elif b <= 10000:
+                reasonable_b = 1000000
+                batch_commands = batch_commands = ["-b "+str(b) +" -end_size "+str(x + reasonable_b) +" -start_size " + str(x) for x in range(0, num_lines, reasonable_b)]
               else:
-                num_lines = sum(1 for line in open(read_dir + filename))
-                #batch_commands = ["-b "+str(b) +" -end_size "+str(x + 100*b) +" -start_size " + str(x) for x in range(0, num_lines, 100*b)]
                 batch_commands = ["-start_size "+str(x)+" -num_dynamic_edges "+str(x + b) for x in range(0, num_lines, b)]
-                #if (num_lines % b != 0):
-                #  batch_commands.append("-num_dynamic_edges " + str(num_lines))
               for bc in batch_commands:
                 ss = ("PARLAY_NUM_THREADS=" + str(nw) + " timeout 6h " + program_dir + program + " -s -i"
                 " " + read_dir + filename + " -eps " + str(e) + " "
                 "-delta " + str(d) + " " + bc + " "
-                "-rounds 4 " + stats + " " + read_dir + empty)
+                "-rounds " + str(num_rounds) + stats + " " + read_dir + empty)
                 out = shellGetOutput(ss)
                 appendToFile(out, out_filename)
                 time += computeTimeout(out)
