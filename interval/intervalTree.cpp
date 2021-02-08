5fa854da2daa4a5ec0629b4146b8b9a1852bfaaf
Laxman Dhulipala <laxmandhulipala@gmail.com>
2021-02-08 18:14:58 -0500

    multiinsert in interval_tree

--- interval/intervalTree.cpp
+++ interval/intervalTree.cpp
@@ -54,6 +54,9 @@ struct interval_map {
 //  }
 
   void insert(interval i) {m.insert(i);}
+  void insertBatch(const parlay::sequence<interval>& updates) {
+    m = amap::multi_insert(std::move(m), updates);
+  }
 
   vector<interval> report_all(point p) {
     vector<interval> vec;
@@ -138,13 +141,82 @@ void test_all(size_t n, size_t q_num, size_t rounds) {
 
 }
 
+void test_updates(size_t n, size_t m, size_t rounds) {
+  parlay::sequence<par> v(n);
+  parlay::sequence<par> vv(n);
+  size_t max_size = (((size_t) 1) << 31)-1;
+
+  parlay::random r(0);
+  parlay::parallel_for(0, n, [&] (size_t i) {
+    point start = r.ith_rand(2*i)%(max_size/2);
+    point end = start + r.ith_rand(2*i+1)%(max_size-start);
+    v[i] = make_tuple(start, end);
+  });
+
+  parlay::sequence<par> updates(m);
+  r = r.next();
+  parlay::parallel_for(0, m, [&] (size_t i) {
+    point start = r.ith_rand(2*i)%(max_size/2);
+    point end = start + r.ith_rand(2*i+1)%(max_size-start);
+    updates[i] = make_tuple(start, end);
+  });
+
+  parlay::sequence<double> build_tm(rounds);
+  parlay::sequence<double> update_tm(rounds);
+  const size_t threads = parlay::num_workers();
+  for (size_t i=0; i < rounds+1; i++) {
+    {
+      parlay::parallel_for (0, n, [&] (size_t i) {vv[i] = v[i];});
+      interval_map::reserve(n);
+      timer t;
+      t.start();
+      interval_map itree(vv);
+      double tm = t.stop();
+      std::cout << "Finished build in " << tm << std::endl;
+      if (i>0) build_tm[i-1] = tm;
+
+      timer tup;
+      tup.start();
+      itree.insertBatch(updates);
+      double tm2 = tup.stop();
+      std::cout << "Finished updates in " << tm2 << std::endl;
+      if (i>0) update_tm[i-1] = tm2;
+    }
+    interval_map::finish();
+  }
+
+  auto less = [] (double a, double b) {return a < b;};
+  parlay::sort(build_tm,less);
+  parlay::sort(update_tm,less);
+
+  cout << "interval build"
+       << ", threads = " << threads
+       << ", rounds = " << rounds
+       << ", n = " << n
+       << ", m = " << m
+       << ", time = " << build_tm[rounds/2] << endl;
+
+  cout << "interval update"
+       << ", threads = " << threads
+       << ", rounds = " << rounds
+       << ", n = " << n
+       << ", m = " << m
+       << ", time = " << update_tm[rounds/2] << endl;
+}
+
+
 int main(int argc, char** argv) {
   commandLine P(argc, argv,
 		"./intervalTree [-n size] [-q num_queries] [-r rounds]");
   size_t n = P.getOptionLongValue("-n", 100000000);
   size_t q_num = P.getOptionLongValue("-q", 10000000);
   size_t rounds = P.getOptionIntValue("-r", 5);
-  test_all(n, q_num, rounds);
+  if (P.getOptionValue("-run_updates")) {
+    size_t m = P.getOptionIntValue("-m", 10000000);
+    test_updates(n, m, rounds);
+  } else {
+    test_all(n, q_num, rounds);
+  }
 
   return 0;
 }
