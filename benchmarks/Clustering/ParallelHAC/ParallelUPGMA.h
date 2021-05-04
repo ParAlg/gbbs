d9bf4908291a0b1b8982b118472389e3a0af86c9
Laxman Dhulipala <laxmandhulipala@gmail.com>
2021-05-04 03:14:12 -0400

    HAC

--- benchmarks/Clustering/ParallelHAC/ParallelUPGMA.h
+++ benchmarks/Clustering/ParallelHAC/ParallelUPGMA.h
@@ -69,10 +69,21 @@ namespace clustering {
 //
 // So the edges would be stored both in the hash table, and in the trees.
 
+struct vtx_status {
+  vtx_status() {}
+
+  bool is_red() { return red; }
+  bool is_blue() { return !red; }
+
+  bool red;
+};
+
 
 template <class ClusteredGraph, class W>
 void ProcessEdgesComplete(ClusteredGraph& CG, sequence<std::tuple<uintE, uintE, W>>&& edges) {
   // using edge = std::tuple<uintE, uintE, W>;
+
+  auto V = sequence<vtx_status>::uninitialized(CG.n);
 
   // Do we need to sort the edges / construct the graph in CSR format?
   // - need to sample a random neighbor of a vertex (seems weird to implement
@@ -103,6 +114,15 @@ void ProcessEdgesComplete(ClusteredGraph& CG, sequence<std::tuple<uintE, uintE, 
   auto B = S.cut(std::get<1>(Split), S.size());
   std::cout << "I size = " << I.size() << " R size = " << R.size() << " B size = " << B.size() << std::endl;
 
+  // Blue vertices select a random red neighbor to try and propose to.
+  // Red vertices compute an MIS on their desired neighbors. Conflict edges are
+  // edges with weight < threshold.
+  //
+  // Idea: just pick local minima (if there are conflict edges).
+
+  // Per vertex array stores:
+  // - red/blue identification
+
 
 }
 
@@ -123,52 +143,75 @@ auto ParallelUPGMA(symmetric_graph<w_vertex, IW>& G, Weights& weights, double ep
   using W = typename Weights::weight_type;
   using edge = std::tuple<uintE, uintE, W>;
 
-  // For log_{1+eps}(...) rounds:
-  //   - extract active edges in a sequence
-  //   - process active edges to obtain a sub-clustering at this level
-  // - emit overall clustering
-
-  //auto degrees = sequence<size_t>::from_function(CG.n, [&] (size_t i) { return CG.degree(i); });
-  //size_t sum_degs = scan_inplace(make_slice(degrees));
-  //std::cout << "sum_degs = " << sum_degs << std::endl;
-
-  // Map-reduce to figure out the number of edges with weight > threshold.
-  auto wgh_degrees = sequence<size_t>::from_function(CG.n, [&] (size_t i) {
-    // should only process active clusters
-    auto pred_f = [&] (const uintE& u, const uintE& v, const W& wgh) {
-      // check if wgh > threshold for this round. replace first cond with this.
-      // symmetry break.
-      return true && (u < v);
-    };
-    return CG.clusters[i].countNeighbors(i, pred_f);
-  });
-  size_t sum_wgh_degrees = scan_inplace(make_slice(wgh_degrees));
-  std::cout << "sum_wgh_degs = " << sum_wgh_degrees << std::endl;
-
-  // Extract the edges.
-  auto edges = sequence<edge>::uninitialized(sum_wgh_degrees);
+  W max_weight = (W)0;
+  W min_weight = std::numeric_limits<W>::max();
   parallel_for(0, CG.n, [&] (size_t i) {
-    size_t offset = wgh_degrees[i];
-    size_t delta = ((i == CG.n-1) ? sum_wgh_degrees : wgh_degrees[i+1]) - offset;
-    if (delta > 0) {
-      size_t k = 0;
-      auto map_f = [&] (const uintE& u, const uintE& v, const W& wgh) {
-        edges[offset + k] = {u, v, wgh};
-        k++;
-      };
-      CG.clusters[i].iterate(i, map_f);
-    }
+    auto f = [&] (const uintE& u, const uintE& v, const W& wgh) {
+      if (wgh > max_weight) { pbbslib::write_max(&max_weight, wgh); }
+      if (wgh < min_weight) { pbbslib::write_min(&max_weight, wgh); }
+    };
+    CG.clusters[i].iterate(i, f);
   });
+  assert(max_weight >= min_weight);
+  std::cout << "Max weight = " << max_weight << " Min weight = " << min_weight << std::endl;
 
-  // All edges passing the threshold for this round now in edges. Now process
-  // using random-mate-based algorithm.
-  //
-  // The stuff above is generic for any linkage, but what comes next is specific
-  // to the given linkage function.
+  double one_plus_eps = 1 + epsilon;
+  long rounds = max((size_t)ceil(log(max_weight / min_weight) / log(one_plus_eps)), (size_t)1);
 
-  ProcessEdgesComplete(CG, std::move(edges));
+  while (rounds > 0) {
+    W lower_threshold = max_weight / one_plus_eps;
+
+    // Map-reduce to figure out the number of edges with weight > threshold.
+    auto wgh_degrees = sequence<size_t>::from_function(CG.n, [&] (size_t i) {
+      // should only process active clusters
+      auto pred_f = [&] (const uintE& u, const uintE& v, const W& wgh) {
+        assert(wgh <= max_weight);
+        return (wgh >= lower_threshold) && (u < v);   // predicate
+      };
+      return CG.clusters[i].countNeighbors(i, pred_f);
+    });
+    size_t sum_wgh_degrees = scan_inplace(make_slice(wgh_degrees));
+    std::cout << "sum_wgh_degs = " << sum_wgh_degrees << std::endl;
+
+    // Extract the edges.
+    auto edges = sequence<edge>::uninitialized(sum_wgh_degrees);
+    parallel_for(0, CG.n, [&] (size_t i) {
+      size_t offset = wgh_degrees[i];
+      size_t delta = ((i == CG.n-1) ? sum_wgh_degrees : wgh_degrees[i+1]) - offset;
+      if (delta > 0) {
+        size_t k = 0;
+        auto map_f = [&] (const uintE& u, const uintE& v, const W& wgh) {
+          if ((wgh >= lower_threshold) && (u < v)) {  // predicate
+            edges[offset + k] = {u, v, wgh};
+            k++;
+          }
+        };
+        CG.clusters[i].iterate(i, map_f);
+      }
+    });
+
+    // All edges passing the threshold for this round now in edges. Now process
+    // using random-mate-based algorithm.
+    //
+    // The stuff above is generic for any linkage, but what comes next is specific
+    // to the given linkage function.
+    ProcessEdgesComplete(CG, std::move(edges));
+
+    max_weight /= one_plus_eps;
+  }
+
 }
 
+
+
+// For log_{1+eps}(...) rounds:
+//   - extract active edges in a sequence
+//   - process active edges to obtain a sub-clustering at this level
+// - emit overall clustering
+
+//auto degrees = sequence<size_t>::from_function(CG.n, [&] (size_t i) { return CG.degree(i); });
+//size_t sum_degs = scan_inplace(make_slice(degrees));
+//std::cout << "sum_degs = " << sum_degs << std::endl;
 
 
 //  size_t n = G.n;
