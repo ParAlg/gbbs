2ed9905afece5bf21f1efedc436fd21b04fb6bfa
Jessica Shi <jessicashi@gmail.com>
2021-05-19 15:07:00 -0400

    bug

--- benchmarks/NucleusDecomposition/NucleusDecomposition.h
+++ benchmarks/NucleusDecomposition/NucleusDecomposition.h
@@ -214,7 +214,11 @@ size_t k, size_t max_deg, bool label, F get_active, size_t active_size,
     // check that base[0] to base[k+1] are all edges
     for (int i = 0; i < k + 1; i++) {
       int i1 = (i + 1) % (k + 1);
-      assert(is_edge(G, base[i], base[i+1]));
+      if (!is_edge(G, base[i], base[i1])) {
+        std::cout << "i: " << i << ", i1: " << i1 << ", base i: "<< base[i] << ", base i1: " << base[i1] << std::endl;
+        fflush(stdout);
+      }
+      assert(is_edge(G, base[i], base[i1]));
 
     }
     cliques->extract_indices(base, is_active, is_inactive, [&](std::size_t index, double val){
