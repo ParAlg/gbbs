4bb4727145b5f038573b05a124922266c82d2b89
Jessica Shi <jessicashi@gmail.com>
2021-05-19 15:44:41 -0400

    twotable

--- benchmarks/NucleusDecomposition/twotable_nosearch.h
+++ benchmarks/NucleusDecomposition/twotable_nosearch.h
@@ -117,7 +117,7 @@ namespace twotable_nosearch {
         // Modify top_table_sizes2 to be appropriately oversized
         parallel_for(0, top_table_sizes2.size(), [&](std::size_t i) {
           auto m = 1 + ((size_t)1 << pbbslib::log2_up((size_t)(1.1 * std::get<1>(top_table_sizes2[i])) + 1));
-          actual_sizes[i] = m;
+          actual_sizes[i] = m + 1;
         });
         actual_sizes[top_table_sizes2.size()] = 0;
         // Do a scan inplace
