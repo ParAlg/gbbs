7786db150fabb1bf3340c534be31053c597ec14e
Jessica Shi <jessicashi@gmail.com>
2021-05-20 13:30:04 -0400

    check intersect nucleus

--- benchmarks/NucleusDecomposition/NucleusDecomposition.h
+++ benchmarks/NucleusDecomposition/NucleusDecomposition.h
@@ -213,7 +213,8 @@ size_t k, size_t max_deg, bool label, F get_active, size_t active_size,
   T* cliques, size_t n, list_buffer& count_idxs, timer& t1) {
 
   // Set up space for clique counting
-  auto init_induced = [&](HybridSpace_lw* induced) { induced->turn_on_check(); induced->alloc(max_deg, k-r, G.n, true, true, true); };
+  auto init_induced = [&](HybridSpace_lw* induced) { induced->alloc(max_deg, k-r, G.n, true, true, true); };
+  //induced->turn_on_check(); 
   auto finish_induced = [&](HybridSpace_lw* induced) { if (induced != nullptr) { delete induced; } };
 
   // Mark every vertex in the active set
