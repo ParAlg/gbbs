13ac25346604b8716a0351b7bea597bbe03e7f54
Jessica Shi <jessicashi@gmail.com>
2021-05-19 21:22:10 -0400

    fix

--- benchmarks/NucleusDecomposition/NucleusDecomposition.h
+++ benchmarks/NucleusDecomposition/NucleusDecomposition.h
@@ -398,13 +398,13 @@ sequence<bucket_t> Peel(Graph& G, Graph2& DG, size_t r, size_t k,
           //assert(std::modf(ppc[v], &intpart) == 0.0);
         //bucket_t deg = D[v];
         bucket_t deg = cliques->get_count(v);
-        auto val = cliques->update_count(v, (size_t) ppc[v]);
-        ppc[v] = 0;
+        
         if (deg <= cur_bkt) {
           std::cout << "Deg: " << deg << ", bkt: " << cur_bkt << std::endl;
         }
-        assert(deg >= cur_bkt);
-        if (deg >= cur_bkt) {
+        assert(deg > cur_bkt);
+        if (deg > cur_bkt) {
+          auto val = cliques->update_count(v, (size_t) ppc[v]);
           bucket_t new_deg = std::max((bucket_t) val, (bucket_t) cur_bkt);
           //D[v] = new_deg;
           // store (v, bkt) in an array now, pass it to apply_f below instead of what's there right now -- maybe just store it in D_filter?
@@ -412,6 +412,7 @@ sequence<bucket_t> Peel(Graph& G, Graph2& DG, size_t r, size_t k,
           D_filter[i] = std::make_tuple(v, b.get_bucket(deg, new_deg));
         } else D_filter[i] = std::make_tuple(num_entries + 1, 0);
         }
+        ppc[v] = 0;
       };
     t_count.start();
      filter_size = cliqueUpdate(G, DG, r, k, max_deg, true, get_active, active_size, 
