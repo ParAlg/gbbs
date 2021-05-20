bd6f3c243013bd600aa0d0c5b8598e17e7e1b845
Jessica Shi <jessicashi@gmail.com>
2021-05-20 11:57:16 -0400

    serializing

--- benchmarks/NucleusDecomposition/NucleusDecomposition.h
+++ benchmarks/NucleusDecomposition/NucleusDecomposition.h
@@ -259,8 +259,10 @@ t1.start();
   //assert(k-r == 1);
   //parallel_for_alloc<HybridSpace_lw>(init_induced, finish_induced, 0, active_size,
   //                                   [&](size_t i, HybridSpace_lw* induced) {
-  parallel_for(0, active_size, [&](size_t i){
-    HybridSpace_lw* induced = new HybridSpace_lw();
+  //parallel_for(0, active_size, [&](size_t i){
+  HybridSpace_lw* induced = new HybridSpace_lw();
+  for (std::size_t i = 0; i < active_size; i++ ){
+    
     init_induced(induced);
 
     // TODO: THIS PART IS WRONG
@@ -290,8 +292,9 @@ t1.start();
     NKCliqueDir_fast_hybrid_rec(DG, 1, k-r, induced, update_d, base);
 
     induced->worker_in_use = UINT_E_MAX;
-    finish_induced(induced);
-  }, 1, true); //granularity
+    
+  }//, 1, true); //granularity
+  finish_induced(induced);
   //std::cout << "End setup nucleus" << std::endl; fflush(stdout);
 t1.stop();
 
