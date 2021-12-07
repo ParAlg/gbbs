c6d68d212fbc18d79b3439d8c9fc876b543af05f
Jessica Shi <jessicashi@gmail.com>
2021-12-06 22:12:20 -0500

    five cycle sparsify attempt

--- benchmarks/CycleCounting/Kowalik5Cycle/FiveCycle.cc
+++ benchmarks/CycleCounting/Kowalik5Cycle/FiveCycle.cc
@@ -1,20 +1,9 @@
 #include "FiveCycle.h"
 
 namespace gbbs {
+
 template <class Graph>
-double Count5Cycle_runner(Graph& G, commandLine P) {
-  std::cout << "ULONG_MAX: " << ULONG_MAX << std::endl;
-  std::cout << "### Application: Cycle" << std::endl;
-  std::cout << "### Graph: " << P.getArgument(0) << std::endl;
-  std::cout << "### n: " << G.n << std::endl;
-  std::cout << "### m: " << G.m << std::endl;
-  std::cout << "### ------------------------------------" << std::endl;
-  assert(P.getOption("-s")); // make sure input graph is symmetric
-  bool serial = P.getOptionValue("--serial"); 
-  bool no_schedule = P.getOptionValue("--no-schedule");
-  bool experiment = P.getOptionValue("--exp");
-  bool escape = P.getOptionValue("--escape");
-  long order_type = P.getOptionLongValue("-o", 0); 
+std::tuple<ulong, double> Count5Cycle_runner(Graph& G, long order_type, bool experiment, bool escape, bool no_schedule, bool serial) {
   std::cout << "### Direct Type: "; 
   if (order_type == 0){
     std::cout << "Goodrich-Pszona" << std::endl;
@@ -55,7 +44,50 @@ double Count5Cycle_runner(Graph& G, commandLine P) {
   std::cout << "### Number of Cycles: " << numCycles << std::endl;
   std::cout << "### Running Time: " << tt << std::endl;
 
-  return tt;
+  return std::make_tuple(numCycles, tt);
+}
+
+template <class Graph>
+double Count5Cycle_runner(Graph& G, commandLine P) {
+  std::cout << "ULONG_MAX: " << ULONG_MAX << std::endl;
+  std::cout << "### Application: Cycle" << std::endl;
+  std::cout << "### Graph: " << P.getArgument(0) << std::endl;
+  std::cout << "### n: " << G.n << std::endl;
+  std::cout << "### m: " << G.m << std::endl;
+  std::cout << "### ------------------------------------" << std::endl;
+  assert(P.getOption("-s")); // make sure input graph is symmetric
+
+  bool sparsify = P.getOptionValue("--sparse"); // if set, use colorful sparsification for approx counting
+  long sparsify_denom = P.getOptionLongValue("--colors", 0); // number of colors for colorful sparsification
+  long esparsify_denom = P.getOptionLongValue("--edenom", 0); // probability of keeping an edge for edge sparsification
+
+  bool serial = P.getOptionValue("--serial"); 
+  bool no_schedule = P.getOptionValue("--no-schedule");
+  bool experiment = P.getOptionValue("--exp");
+  bool escape = P.getOptionValue("--escape");
+  long order_type = P.getOptionLongValue("-o", 0);
+
+  if (sparsify) {
+      // Colorful sparsify graph, with random seed
+    if (sparsify_denom != 0) {
+      auto G_sparse = clr_sparsify_graph(G, sparsify_denom, 7398234);
+      auto ret = Count5Cycle_runner(G_sparse, order_type, experiment, escape, no_schedule, serial);
+      auto count = std::get<0>(ret);
+      count = count * pow(sparsify_denom,4); // TODO: fix
+      std::cout << "un-sparse count: " << count << std::endl;
+      return std::get<1>(ret);
+    } else if (esparsify_denom != 0) {
+      // edge sparsify
+      auto G_sparse = edge_sparsify_graph(G, esparsify_denom, 7398234);
+      auto ret = Count5Cycle_runner(G_sparse, order_type, experiment, escape, no_schedule, serial);
+      auto count = std::get<0>(ret);
+      count = count * pow(esparsify_denom,5); // TODO: fix
+      std::cout << "un-sparse count: " << count << std::endl;
+      return std::get<1>(ret);
+    }
+  }
+  
+  return std::get<1>(Count5Cycle_runner(G, order_type, experiment, escape, no_schedule, serial));
 }
 }  // namespace gbbs
 
