ec415775d572373f6bc05776424022eba93bc04d
Jessica Shi <jessicashi@gmail.com>
2022-02-06 22:29:58 -0500

    twotable_nosearch fix

--- benchmarks/NucleusDecomposition/NucleusDecomposition.h
+++ benchmarks/NucleusDecomposition/NucleusDecomposition.h
@@ -238,7 +238,7 @@ inline void NucleusDecomposition(Graph& GA, size_t r, size_t s, long table_type,
 
   int num_bits_in_n = 1 + parlay::log2_up(DG.n + 1); //32
   int num_bytes_needed = round_up<int>(((std::max(static_cast<int>(1), 
-    static_cast<int>(r - (num_levels - 1))) * num_bits_in_n) + 1), 8);
+    static_cast<int>(r - (1 - 1))) * num_bits_in_n) + 1), 8); // num_levels - 1
   int shift_factor = num_bits_in_n; //32
 
   std::cout << "Num bytes needed: " << num_bytes_needed << std::endl;
