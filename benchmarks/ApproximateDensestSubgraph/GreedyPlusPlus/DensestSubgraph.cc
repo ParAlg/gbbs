c37cf6f4470041a37c9e3240ecf89111465c506a
Quanquan Liu <qliu2011@gmail.com>
2022-08-14 17:03:13 -0400

    Greedy++

---
+++ benchmarks/ApproximateDensestSubgraph/GreedyPlusPlus/DensestSubgraph.cc
@@ -0,0 +1,63 @@
+// This code is part of the project "Theoretically Efficient Parallel Graph
+// Algorithms Can Be Fast and Scalable", presented at Symposium on Parallelism
+// in Algorithms and Architectures, 2018.
+// Copyright (c) 2018 Laxman Dhulipala, Guy Blelloch, and Julian Shun
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all  copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Usage:
+// numactl -i all ./DensestSubgraph -rounds 3 -s -m com-orkut.ungraph.txt_SJ
+// flags:
+//   required:
+//     -s : indicates that the graph is symmetric
+//   optional:
+//     -m : indicate that the graph should be mmap'd
+//     -c : indicate that the graph is compressed
+//     -rounds : the number of times to run the algorithm
+
+#include "DensestSubgraph.h"
+
+namespace gbbs {
+namespace {
+
+template <class Graph>
+double DensestSubgraph_runner(Graph& G, commandLine P) {
+  double eps = P.getOptionDoubleValue("-eps", 0.001);
+  std::cout << "### Application: DensestSubgraph" << std::endl;
+  std::cout << "### Graph: " << P.getArgument(0) << std::endl;
+  std::cout << "### Threads: " << num_workers() << std::endl;
+  std::cout << "### n: " << G.n << std::endl;
+  std::cout << "### m: " << G.m << std::endl;
+  std::cout << "### Params: -eps = " << eps << std::endl;
+  std::cout << "### ------------------------------------" << std::endl;
+  assert(P.getOption("-s"));
+
+  timer t;
+  t.start();
+  GreedyPlusPlusDensestSubgraph(G);
+  double tt = t.stop();
+
+  std::cout << "### Running Time: " << tt << std::endl;
+  return tt;
+}
+
+}  // namespace
+}  // namespace gbbs
+
+generate_main(gbbs::DensestSubgraph_runner, false);
---
+++ benchmarks/ApproximateDensestSubgraph/GreedyPlusPlus/README.md
@@ -0,0 +1,12 @@
+This package implements the 2-approximate densest subgraph algorithm
+by Charikar.
+
+Reference:
+@inproceedings{charikar2000greedy,
+  title={Greedy approximation algorithms for finding dense components in a graph},
+  author={Charikar, Moses},
+  booktitle={International Workshop on Approximation Algorithms for Combinatorial Optimization},
+  pages={84--95},
+  year={2000},
+  organization={Springer}
+}
