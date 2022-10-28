d748fdb76784a7241c610023aed4a073d5f7663b
Jessica Shi <jessicashi@gmail.com>
2022-10-28 11:24:47 -0400

    trying to fix

--- benchmarks/NucleusDecomposition/NucleusDecomposition_structs.h
+++ benchmarks/NucleusDecomposition/NucleusDecomposition_structs.h
@@ -120,8 +120,14 @@ void EfficientConnectWhilePeeling::link(X a, Y b, F& cores) {
   if (cores(a) <= cores(b)) {
     auto link_a = links[a]; auto link_b = links[b];
     if (link_a != UINT_E_MAX && link_b != UINT_E_MAX) this->link(link_a, link_b, cores);
-    if (link_a != UINT_E_MAX) this->link(link_a, b, cores);
-    if (link_b != UINT_E_MAX) this->link(link_b, a, cores);
+    if (link_a != UINT_E_MAX) {
+      uintE parent = simple_union_find::find_compress_atomic(b, this->uf.parents);
+      if (parent != b) this->link(link_a, parent, cores);
+    }
+    if (link_b != UINT_E_MAX) {
+      uintE parent = simple_union_find::find_compress_atomic(a, this->uf.parents);
+      if (parent != a) this->link(link_b, parent, cores);
+    }
   }
   if (cores(a) == cores(b)) {
     this->uf.unite(a, b);
@@ -146,8 +152,8 @@ void EfficientConnectWhilePeeling::link(X a, Y b, F& cores) {
     }
   }
   else if (cores(a) < cores(b)) {
-    if (b != uf.parents[b]) this->link(a, uf.parents[b], cores);
-    else {
+    //if (b != uf.parents[b]) this->link(a, uf.parents[b], cores);
+    //else {
       if (!gbbs::atomic_compare_and_swap<uintE>(&(links[b]), UINT_E_MAX, a)) {
       while (true) {
         gbbs::uintE c = links[b];
@@ -166,7 +172,7 @@ void EfficientConnectWhilePeeling::link(X a, Y b, F& cores) {
       } else {
         if (b != uf.parents[b]) this->link(a, uf.parents[b], cores);
       }
-    }
+    //}
   }
   else {
     this->link(b, a, cores);
