a27dd68894559bc19bed31c0901344e4db92fd95
Jessica Shi <jessicashi@gmail.com>
2022-10-28 21:02:52 -0400

    trying to fix

--- benchmarks/NucleusDecomposition/NucleusDecomposition_structs.h
+++ benchmarks/NucleusDecomposition/NucleusDecomposition_structs.h
@@ -176,6 +176,9 @@ void EfficientConnectWhilePeeling::link(X a, Y b, F& cores) {
     }
     auto link_a = links[a]; //auto link_b = links[b];
     if (link_a != UINT_E_MAX) {
+      while (link_a != UINT_E_MAX && !gbbs::atomic_compare_and_swap<uintE>(&(links[a]), link_a, UINT_E_MAX)){
+        link_a = links[a];
+      }
       this->link(link_a, b, cores);
       //uintE parent = simple_union_find::find_compress_atomic(b, this->uf.parents);
       //if (parent != b) this->link(link_a, parent, cores);
