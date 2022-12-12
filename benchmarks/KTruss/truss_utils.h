83742fdd203ed52275050115a4fc671658314f53
Jessica Shi <jessicashi@gmail.com>
2022-12-12 15:28:52 -0500

    ktruss

--- benchmarks/KTruss/truss_utils.h
+++ benchmarks/KTruss/truss_utils.h
@@ -488,26 +488,18 @@ void decrement_trussness(Graph& G, edge_t id, uintE u, uintE v,
     if (should_remove(k, trussness_uv, trussness_uw, trussness_vw, uv_id, uw_id,
                       vw_id, still_active)) {
       ctr++;
-      if (trussness_uw > k) {
+      //if (trussness_uw > k) {
         decrement_tab.insert_f(std::make_tuple(uw_id, (uintE)1), add_f);
         //          decrement_tab.insert(uw_id);
-      }
-      if (trussness_vw > k) {
+      //}
+      //if (trussness_vw > k) {
         decrement_tab.insert_f(std::make_tuple(vw_id, (uintE)1), add_f);
         //          decrement_tab.insert(vw_id);
-      }
+      //}
     }
   };
     auto v_v = G.get_vertex(v).out_neighbors();
-    if (!use_pnd) {
-      G.get_vertex(u).out_neighbors().intersect_f_par(&v_v, f);
-    } else {
-      std::vector<uintE> inter;
-      truss_intersectionPND(G, v, u, inter);
-      for (std::size_t p = 0; p < inter.size(); p++) {
-        f(u, v, inter[p]);
-      }
-    }
+    G.get_vertex(u).out_neighbors().intersect_f_par(&v_v, f);
   }
 
 
