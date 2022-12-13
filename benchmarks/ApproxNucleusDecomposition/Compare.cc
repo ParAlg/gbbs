478cf6f6aad775218fedcee30b4aefc86cc3409f
Jessica Shi <jessicashi@gmail.com>
2022-12-13 11:11:02 -0500

    comparek truss

--- benchmarks/ApproxNucleusDecomposition/Compare.cc
+++ benchmarks/ApproxNucleusDecomposition/Compare.cc
@@ -35,7 +35,7 @@ sequence<std::pair<std::vector<gbbs::uintE>, gbbs::uintE>> read_cores_ktruss(std
     split(line, row_values.first);
     row_values.second = row_values.first.back();
     row_values.first.pop_back();
-    if (row_values.first[1] > row_values.first[0]) {
+    if (row_values.first[1] > row_values.first[0] && row_values.second != 0) {
       std::sort(row_values.first.begin(), row_values.first.end());
       cores[index] = row_values;
       index++;
