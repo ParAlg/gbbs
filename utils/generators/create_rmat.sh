199adc05b370cf5333fe2bf9afdee5127c01edcd
Shangdi <shangdiy@mit.edu>
2023-12-06 11:31:06 +0000

    change sh to use bazel

--- utils/generators/create_rmat.sh
+++ utils/generators/create_rmat.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-#nums=(1048576 4194304 16777216 268435456 1073741824)
-nums=(1048576)
-edge_nums=(1 10 100 1000 10000 100000 1000000 2000000)
-output_dir="/ssd0/graphs/edge_streams_coo"
+
+# nums=(32768)
+# edge_nums=(32768000)
+# nums=(2097152)
+# edge_nums=(209715200)
+# nums=(1048576)
+# edge_nums=(1048576000)
+nums=(16777216)
+edge_nums=(1677721600)
+output_dir="/home/sy/gbbs/utils/generators"
 for num in ${nums[@]}
 do
   for edge_num in ${edge_nums[@]}
   do
-    echo "numactl -i all ./RMAT -n $num -m $edge_num -outfile $output_dir/rmat_$num\_$edge_num"
-    numactl -i all ./RMAT -n $num -m $edge_num -outfile $output_dir/rmat_$num\_$edge_num
+    echo "bazel run utils/generators:RMAT -- -n $num -m $edge_num -outfile $output_dir/rmat_$num\_$edge_num"
+    bazel run utils/generators:RMAT -- -n $num -m $edge_num -outfile $output_dir/rmat_$num\_$edge_num
   done
 done
